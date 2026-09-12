7f54df4c6619fdeedba57f3a70910cb5b99d75d9
RudraRM <Rushil.rudraksha@gmail.com>
2026-09-11 21:00:46 -0500

    Delete app directory

--- app/layout.tsx
+++
@@ -1,4 +0,0 @@
-import "./globals.css";
-import { AudioProvider } from "@/components/AudioProvider";
-export const metadata = { title: "Jarvis Mark 85", description: "Voice-operated neural interface" };
-export default function Layout({children}:{children:React.ReactNode}) { return <html lang="en"><body><AudioProvider>{children}</AudioProvider></body></html> }
